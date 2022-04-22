2b9be8780468b7b3eab2f40a270386346d811a61
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-22 15:05:28 +0200

    Add all join query exept the bonus one

--- MySQL.session.sql
+++ MySQL.session.sql
@@ -1,40 +1,38 @@
 --  GROUP BY QUERIES
---
+
 -- @block
 -- 1. Contare quanti iscritti ci sono stati ogni anno
--- Count how many students have been enrolled in each year
+
 SELECT YEAR(enrolment_date) , COUNT(*)
 FROM students
 GROUP BY YEAR(enrolment_date);
 
 -- @block
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
--- Count the number of teachers that have the office in the same building
+
 SELECT office_address, COUNT(*)
 FROM teachers
 GROUP BY office_address;
 
 -- @block
 -- 3. Calcolare la media dei voti di ogni appello d'esame
--- Calculate the average of each exam session
+
 SELECT exam_id, AVG(vote)
 FROM exam_student
 GROUP BY exam_id;
 
 -- @BLOCK
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
--- Count the number of degrees per department
+
 SELECT department_id, COUNT(*)
 FROM degrees
 GROUP BY department_id;
 
 -- JOIN QUERIES
--- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
--- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica (54)
--- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
 -- @BLOCK
 -- 1. Selezionare tutti gli studenti iscritti al Corso di Laurea in Economia
--- select all students enrolled in 'Corso di Laurea in Economia' degree
+
 SELECT students.id, students.name, students.surname, students.registration_number, students.email
 FROM students
 JOIN degrees
@@ -43,7 +41,7 @@ WHERE degrees.name = 'Corso di Laurea in Economia';
 
 -- @BLOCK
 -- 2. Selezionare tutti i Corsi di Laurea del Dipartimento di Neuroscienze
--- select all degrees of 'Dipartimento di Neuroscienze' department
+
 SELECT degrees.name
 FROM degrees
 JOIN departments
@@ -52,7 +50,7 @@ WHERE departments.name = 'Dipartimento di Neuroscienze';
 
 -- @BLOCK
 -- 3. Selezionare tutti i corsi in cui insegna Fulvio Amato (id=44)
--- select all courses in which Fulvio Amato (id=44) teaches
+
 SELECT courses.name
 FROM courses
 JOIN course_teacher
@@ -69,3 +67,39 @@ ON students.degree_id = degrees.id
 JOIN departments
 ON degrees.department_id = departments.id
 ORDER BY students.surname, students.name;
+
+-- @BLOCK
+-- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
+
+SELECT degrees.name AS degree, courses.name AS course, CONCAT(teachers.name, ' ', teachers.surname) AS teacher
+FROM degrees
+JOIN courses
+ON degrees.id = courses.degree_id
+JOIN course_teacher
+ON courses.id = course_teacher.course_id
+JOIN teachers
+ON course_teacher.teacher_id = teachers.id
+ORDER BY degrees.name, courses.name;
+
+-- @BLOCK
+-- 6. Selezionare tutti i docenti che insegnano nel Dipartimento di Matematica
+
+SELECT DISTINCT(teachers.id), teachers.name, teachers.surname
+FROM teachers
+JOIN course_teacher
+ON teachers.id = course_teacher.teacher_id
+JOIN courses
+ON course_teacher.course_id = courses.id
+JOIN degrees
+ON courses.degree_id = degrees.id
+JOIN departments
+ON degrees.department_id = departments.id
+WHERE departments.name = 'Dipartimento di Matematica'
+
+-- @BLOCK
+-- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
+
+SELECT students.id, students.name, students.surname, COUNT(*)
+FROM students
+JOIN exam_student
+ON students.id = exam_student.student_id;
