b22deae5bea4899aa2bc4c60675a86e636b24ca1
AlexViti <96179738+AlexViti@users.noreply.github.com>
2022-04-22 15:33:26 +0200

    Add bonus

--- MySQL.session.sql
+++ MySQL.session.sql
@@ -99,7 +99,13 @@ WHERE departments.name = 'Dipartimento di Matematica'
 -- @BLOCK
 -- 7. BONUS: Selezionare per ogni studente quanti tentativi d’esame ha sostenuto per superare ciascuno dei suoi esami
 
-SELECT students.id, students.name, students.surname, COUNT(*)
+SELECT students.id, students.name, students.surname, courses.name, COUNT(*) AS attempts
 FROM students
 JOIN exam_student
-ON students.id = exam_student.student_id;
+ON students.id = exam_student.student_id
+JOIN exams
+ON exam_student.exam_id = exams.id
+JOIN courses
+ON exams.course_id = courses.id
+GROUP BY students.id, courses.id
+ORDER BY students.surname, students.name;
